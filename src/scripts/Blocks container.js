e060668b298aed7afe3b3c0e14dd3d45436e7b3f
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2019-12-03 23:50:08 +0300

    add formAContainerOfBlocks

--- src/scripts/Blocks container.js
+++ src/scripts/Blocks container.js
@@ -1,15 +1,40 @@
-function createPartitions(rows, columns, container) {
+function formAContainerOfBlocks(rows, columns, containerId, blockId){
+    let blocksContainer = document.getElementById(containerId),
+        count = createPartitions(rows, columns, blocksContainer, blockId),
+        arr = createColorBlocks(count),
+        shuffleArr = shuffle(arr);
+    fillBlocksInContainer(shuffleArr, blocksContainer);
+    console.log(arr);
+    console.log(shuffleArr);
+    console.log(arr.length);
+}
+function createPartitions(rows, columns, container, blockId) {
+    let count = rows*columns;
     container.style.gridTemplateRows = `repeat(${rows}, 1fr)`; 
     container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
-    for (let i = 0; i < rows*columns; i++) {
+    for (let i = 0; i < count; i++) {
         let partition = document.createElement('div');
-        partition.className = "partition";
+        partition.className = blockId;
         container.appendChild(partition);
     }
+    return rows*columns;
+}
+window.onload = () => {
+    console.log('hy');
+    formAContainerOfBlocks(10, 8, "blocks-container", "partition");
+}
+function fillBlocksInContainer(arr, container){
+    console.log('fillBlocksInContainer');
+    let childs = container.childNodes;
+    [].forEach.call(childs, (element, index) => {
+        if (index != 0) {
+            if (arr[index-1].hasOwnProperty('color')) {
+                console.log(`index = ${index-1}`);
+                console.log(element);
+                console.log(arr[index-1]);
+                element.style.backgroundColor = arr[index-1].color;
+            }
+        }   
+    })
 }
 
-let doc = document.getElementById('blocks-container');
-createPartitions(8, 10, doc);
-let arr = createColorBlocks(80);
-console.log(shuffle(arr));
-console.log(arr.length);
