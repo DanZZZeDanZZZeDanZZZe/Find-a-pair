98bd596e7eb83a4cd36cc4a9684ef9997f26c9b7
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2019-12-17 00:33:10 +0300

    add levels

--- src/scripts/Game.js
+++ src/scripts/Game.js
@@ -60,7 +60,48 @@ let gameLevelConfiguration = [
         rotate: false
     },
     level4 = {
-        name: 'Босиком по болоту'
+        id: 'level4',
+        name: 'Маленькая деревушка',
+        colorConf: [150, 150, 150, 0, 0, 0],
+        time: 200,
+        numberOfHealths: 5,
+        levelWidth: 6,
+        levelHeight: 8,
+        coups: true,
+        rotate: false
+    },
+    level5 = {
+        id: 'level5',
+        name: 'Кисельные берега',
+        colorConf: [250, 100, 250, 50, 70, 40],
+        time: 250,
+        numberOfHealths: 4,
+        levelWidth: 8,
+        levelHeight: 8,
+        coups: false,
+        rotate: true
+    },
+    level6 = {
+        id: 'level6',
+        name: 'Без права на ошибку',
+        colorConf: [50, 50, 50, 10, 10, 10],
+        time: 80,
+        numberOfHealths: 1,
+        levelWidth: 5,
+        levelHeight: 5,
+        coups: false,
+        rotate: false
+    },
+    level7 = {
+        id: 'level7',
+        name: 'Водоворот',
+        colorConf: [100, 100, 250, 30, 10, 120],
+        time: 600,
+        numberOfHealths: 5,
+        levelWidth: 6,
+        levelHeight: 6,
+        coups: true,
+        rotate: true
     }
 ]
 function initializeTheGame(levelConfiguration) {
